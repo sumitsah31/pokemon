fa2b33144dfafc681453c09c8bed21ca60e11bc5
sumitsah31 <sumitdashing1@gmail.com>
2022-09-12 19:58:22 +0530

    test1

---
+++ src/components/Pokemonlist.js
@@ -0,0 +1,63 @@
+import React from "react";
+import PokemonCard from "./PokemonCard";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+const Pokemonlist=()=>{
+    const [pokemondata,setpokemondata]=useState([]);
+    const [loading,setLoading]=useState(true);
+    const [url,setUrl]=useState("https://pokeapi.co/api/v2/pokemon/")
+    const [nextUrl,setNextUrl]=useState();
+    const [prevUrl,setPrevUrl]=useState();
+    const [pokeDex,setPokeDex]=useState();
+
+    const pokeFun=async()=>{
+        setLoading(true)
+        const res=await axios.get(url);
+        setNextUrl(res.data.next);
+        setPrevUrl(res.data.previous);
+        getPokemon(res.data.results)
+        setLoading(false)
+    }
+    const getPokemon=async(res)=>{
+       res.map(async(item)=>{
+          const result=await axios.get(item.url)
+          setpokemondata(state=>{
+              state=[...state,result.data]
+              state.sort((a,b)=>a.id>b.id?1:-1)
+              return state;
+          })
+       })   
+    }
+    useEffect(()=>{
+        pokeFun();
+    },[url])
+    return(
+        <>
+            <div className="container">
+                <div className="content">
+                    <PokemonCard pokemon={pokemondata} loading={loading} infoPokemon={poke=>setPokeDex(poke)}/>
+                    
+                    
+                </div>
+                <div className="content2">
+                   <Pokeinfo data={pokeDex}/>
+                </div>
+            </div>
+            <div className="btn-group">
+                        {  prevUrl && <button onClick={()=>{
+                            setpokemondata([])
+                           setUrl(prevUrl) 
+                        }}>Previous</button>}
+
+                        { nextUrl && <button onClick={()=>{
+                            setpokemondata([])
+                            setUrl(nextUrl)
+                        }}>Next</button>}
+
+                    </div>
+        </>
+    )
+}
+export default Pokemonlist;
